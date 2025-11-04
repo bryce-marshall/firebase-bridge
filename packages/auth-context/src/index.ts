735996a4c105ba1a03329b2bd92ef1f45daa437b
Bryce Marshall <dev.brycemarshall@gmail.com>
2025-11-04 16:51:15 +1300

    wip

--- packages/auth-context/src/index.ts
+++ packages/auth-context/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/auth-manager.js';
 export * from './lib/http/types.js';
+export * from './lib/http/util.js';
 export * from './lib/https/https-broker.js';
 export * from './lib/https/https-v1-handler.js';
 export * from './lib/https/https-v2-handler.js';
