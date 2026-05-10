3db55bba5b12b6fe502feabbfbf76c206c66b63b
Bryce Marshall <dev.brycemarshall@gmail.com>
2026-05-11 11:14:02 +1200

    wip

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,8 @@ module.exports = () => ({
     '<rootDir>/packages/firestore-admin',
     '<rootDir>/packages/firestore-functions',
     '<rootDir>/packages/auth-context',
+    '<rootDir>/packages/cloud-storage',
+    '<rootDir>/packages/storage-mock',
     '<rootDir>/firestore-bridge-production',
   ],
 });
