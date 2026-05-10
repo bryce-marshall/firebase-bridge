038f3fa2ac2445b4705cf391470c102829977697
Bryce Marshall <dev.brycemarshall@gmail.com>
2026-05-11 11:16:53 +1200

    wip

--- firestore-bridge-production/jest.setup.cjs
+++ firestore-bridge-production/jest.setup.cjs
@@ -1,3 +1,4 @@
 process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+process.env.FIREBASE_STORAGE_EMULATOR_HOST = 'localhost:9199';
 // Increase default timeout for all production/emulator tests
 jest.setTimeout(7_000);
